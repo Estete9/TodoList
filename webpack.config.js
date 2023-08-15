62e655b128e7d944de10d8d51316d78a984783fd
Esteban <cepalacios@outlook.com>
2023-08-15 15:07:02 -0500

    Add styles to list and example list item with a textarea element inside

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   mode: 'development',
