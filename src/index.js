4bbd86579bd6252225da3cc80755e7cbfd92acf8
Esteban <cepalacios@outlook.com>
2023-08-15 17:08:04 -0500

    Add refresh and return icons and styling to them

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import './style.css';
 import dragBtnSvg from '../assets/more_vert.svg';
+import iconArrows from '../assets/arrow-cycle.svg';
+import returnIcon from '../assets/return-icon.svg';
 
-const dragBtns = Array.from(document.getElementsByClassName('drag-btn'));
-dragBtns.forEach((btn) => {
-  btn.src = dragBtnSvg;
-});
+document.getElementById('icon-arrows').src = iconArrows;
+document.getElementById('return-icon').src = returnIcon;
+class Task {
+  constructor(description, completed, index) {
+    (this.description = description),
+      (this.completed = completed),
+      (this.index = index);
+  }
+}
+
+const tasks = [
+  new Task('wash the dishes', false, 0),
+  new Task('complete the ToDo list project', false, 1),
+];
+const tasksSection = document.getElementById('tasks-section');
+
+for (let i = 0; i < tasks.length; i += 1) {
+  const task = document.createElement('li');
+  task.classList.add('task-item');
+  const taskInnerHTML = `
+    <input type="checkbox" />
+    <div>
+      <label class="label">${tasks[i].description}</label>
+      <textarea>${tasks[i].description}</textarea>
+    </div>
+    <img src="${dragBtnSvg}" class="drag-btn" alt="drag and drop button" />
+  `;
+  task.innerHTML = taskInnerHTML;
+  tasksSection.appendChild(task);
+}
