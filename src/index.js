e680aa806fea62b9827c2c2423bfcb54c369db05
Esteban <cepalacios@outlook.com>
2023-08-15 15:07:02 -0500

    Add styles to list and example list item with a textarea element inside

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import dragBtnSvg from '../assets/more_vert.svg';
+
+const dragBtns = Array.from(document.getElementsByClassName('drag-btn'));
+dragBtns.forEach((btn) => {
+  btn.src = dragBtnSvg;
+});
