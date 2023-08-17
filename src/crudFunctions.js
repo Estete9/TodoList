d3e41c76291bd9e807fa544146a5658b426ce021
Esteban <cepalacios@outlook.com>
2023-08-17 14:43:50 -0500

    Implement logic for the checkbox and update task.completed accordingly

--- src/crudFunctions.js
+++ src/crudFunctions.js
@@ -1,89 +1,126 @@
-import Task from './task.js';
-import updateUI from './updateUI.js';
-
-export default class crudFunctions {
-  constructor() {
-    const localData = localStorage.getItem('tasksStorage');
-    let local = [];
-    try {
-      local = JSON.parse(localData);
-      if (local === null) local = [];
-    } catch (error) {
-      console.warn('local storage is empty');
-      local = [];
-    }
-    this.tasksList = local;
-  }
-
-  makeIndex = () => {
-    const localData = localStorage.getItem('tasksStorage');
-    let local = [];
-
-    try {
-      local = JSON.parse(localData);
-      if (local === null) local = [];
-    } catch (error) {
-      console.warn('local storage is empty');
-      local = [];
-    }
-    this.tasksList = local;
-
-    if (this.tasksList.length <= 0) {
-      return 1;
-    }
-    return this.tasksList.length + 1;
-  };
-
-  addTask = (taskDescription, tasksCollection) => {
-    const localData = localStorage.getItem('tasksStorage');
-    let local = [];
-
-    try {
-      local = JSON.parse(localData);
-      if (local === null) local = [];
-    } catch (error) {
-      console.warn('local storage is empty');
-      local = [];
-    }
-    this.tasksList = local;
-    const task = new Task(taskDescription, this.makeIndex(this.tasksList));
-    // push task to array and add to local storage
-    this.tasksList.push(task);
-    localStorage.setItem('tasksStorage', JSON.stringify(this.tasksList));
-    updateUI(this.tasksList, tasksCollection);
-  };
-
-  deleteTask = (task, tasksCollection) => {
-    const localData = localStorage.getItem('tasksStorage');
-    let local = [];
-
-    try {
-      local = JSON.parse(localData);
-      if (local === null) local = [];
-    } catch (error) {
-      console.warn('local storage is empty');
-      local = [];
-    }
-    this.tasksList = local;
-    // locate the index of the deleted task with the eventId
-    const index = task.id;
-    // remove the element at index - 1 (-1 to match array index)
-    this.tasksList.splice(index - 1, 1);
-    // update array's index and saves to local storage
-    for (let i = 0; i < this.tasksList.length; i += 1) {
-      this.tasksList[i].index = i + 1;
-    }
-    localStorage.setItem('tasksStorage', JSON.stringify(this.tasksList));
-    updateUI(this.tasksList, tasksCollection);
-  };
-
-  updateTask = (task) => {
-    // save changes to task, update task in the array and save to local storage
-    if (task.value !== task.textContent) {
-      const updatedTask = new Task(task.value, task.id);
-      this.tasksList[task.id - 1] = updatedTask;
-      localStorage.setItem('tasksStorage', JSON.stringify(this.tasksList));
-    }
-  };
-}
+import Task from './task.js';
 
+import updateUI from './updateUI.js';
+
+export default class crudFunctions {
+  constructor() {
+    const localData = localStorage.getItem('tasksStorage');
+
+    let local = [];
+
+    try {
+      local = JSON.parse(localData);
+
+      if (local === null) local = [];
+    } catch (error) {
+      console.warn('local storage is empty');
+
+      local = [];
+    }
+
+    this.tasksList = local;
+  }
+
+  makeIndex = () => {
+    const localData = localStorage.getItem('tasksStorage');
+
+    let local = [];
+
+    try {
+      local = JSON.parse(localData);
+
+      if (local === null) local = [];
+    } catch (error) {
+      console.warn('local storage is empty');
+
+      local = [];
+    }
+
+    this.tasksList = local;
+
+    if (this.tasksList.length <= 0) {
+      return 1;
+    }
+
+    return this.tasksList.length + 1;
+  };
+
+  addTask = (taskDescription, tasksCollection) => {
+    const localData = localStorage.getItem('tasksStorage');
+
+    let local = [];
+
+    try {
+      local = JSON.parse(localData);
+
+      if (local === null) local = [];
+    } catch (error) {
+      console.warn('local storage is empty');
+
+      local = [];
+    }
+
+    this.tasksList = local;
+
+    const task = new Task(taskDescription, this.makeIndex(this.tasksList));
+
+    // push task to array and add to local storage
+
+    this.tasksList.push(task);
+
+    localStorage.setItem('tasksStorage', JSON.stringify(this.tasksList));
+
+    updateUI(this.tasksList, tasksCollection);
+  };
+
+  deleteTask = (task, tasksCollection) => {
+    const localData = localStorage.getItem('tasksStorage');
+
+    let local = [];
+
+    try {
+      local = JSON.parse(localData);
+
+      if (local === null) local = [];
+    } catch (error) {
+      console.warn('local storage is empty');
+
+      local = [];
+    }
+
+    this.tasksList = local;
+
+    // locate the index of the deleted task with the eventId
+
+    const index = task.id;
+
+    // remove the element at index - 1 (-1 to match array index)
+
+    this.tasksList.splice(index - 1, 1);
+
+    // update array's index and saves to local storage
+
+    for (let i = 0; i < this.tasksList.length; i += 1) {
+      this.tasksList[i].index = i + 1;
+    }
+
+    localStorage.setItem('tasksStorage', JSON.stringify(this.tasksList));
+
+    updateUI(this.tasksList, tasksCollection);
+  };
+
+  updateTask = (task) => {
+    // save changes to task, update task in the array and save to local storage
+    // if (task.value === task.textContent) {
+    //   return;
+    // }
+    // if()
+    // if (task.value !== task.textContent) {
+    const updatedTask = new Task(task.value, task.id);
+
+    this.tasksList[task.id - 1] = updatedTask;
+
+    localStorage.setItem('tasksStorage', JSON.stringify(this.tasksList));
+    // }
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,49 @@
-import './style.css';
-import iconArrows from '../assets/arrow-cycle.svg';
-import returnIcon from '../assets/return-icon.svg';
-import ToDoCollection from './crudFunctions.js';
-import updateUI from './updateUI.js';
-import taskSelection from './taskSelection.js';
-
-document.getElementById('icon-arrows').src = iconArrows;
-document.getElementById('return-icon').src = returnIcon;
-
-const taskInput = document.querySelector('#input-section > input');
-const localData = localStorage.getItem('tasksStorage');
-let tasks = [];
-let local = [];
-
-try {
-  local = JSON.parse(localData);
-  if (local === null) local = [];
-} catch (error) {
-  console.warn('local storage is empty');
-  local = [];
-}
-tasks = local;
-const tasksCollection = new ToDoCollection(tasks);
-
-taskInput.addEventListener('keypress', (e) => {
-  if (e.keyCode === 13) {
-    tasksCollection.addTask(taskInput.value, tasksCollection);
-    taskInput.value = '';
-  }
-});
-updateUI(tasksCollection.tasksList, tasksCollection);
-taskSelection(tasksCollection);
+import './style.css';
 
+import iconArrows from '../assets/arrow-cycle.svg';
+
+import returnIcon from '../assets/return-icon.svg';
+
+import ToDoCollection from './crudFunctions.js';
+
+import updateUI from './updateUI.js';
+
+import taskSelection from './taskSelection.js';
+
+document.getElementById('icon-arrows').src = iconArrows;
+
+document.getElementById('return-icon').src = returnIcon;
+
+const taskInput = document.querySelector('#input-section > input');
+
+const localData = localStorage.getItem('tasksStorage');
+
+let tasks = [];
+
+let local = [];
+
+try {
+  local = JSON.parse(localData);
+
+  if (local === null) local = [];
+} catch (error) {
+  console.warn('local storage is empty');
+
+  local = [];
+}
+
+tasks = local;
+
+const tasksCollection = new ToDoCollection(tasks);
+
+taskInput.addEventListener('keypress', (e) => {
+  if (e.keyCode === 13) {
+    tasksCollection.addTask(taskInput.value, tasksCollection);
+
+    taskInput.value = '';
+  }
+});
+
+updateUI(tasksCollection.tasksList, tasksCollection);
+
+taskSelection(tasksCollection);
